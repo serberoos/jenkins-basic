a4405549e127232fbb95980ea8aab9e4c4ed9145
JaeGeun.Song <jae99c@gmail.com>
2021-11-03 10:20:50 +0900

    Change gradle project from maven project

--- src/main/java/com/cicddemo/jenkinsbasic/JenkinsBasicApplication.java
+++ src/main/java/com/cicddemo/jenkinsbasic/JenkinsBasicApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.cicddemo.jenkinsbasic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class JenkinsBasicApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(JenkinsBasicApplication.class, args);
 	}
 
 }
